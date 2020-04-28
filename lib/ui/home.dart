939c17485275174e293fa21251ad84f694fefe1f
tboyd4 <tboyd1898@gmail.com>
2020-04-27 19:46:35 -0500

    added fonts, and additional styles

---
+++ lib/ui/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.deepOrangeAccent,
+      child: Text("Pizza", 
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 80.0,
+          decoration: TextDecoration.none,
+          fontFamily: 'Oxygen',
+          fontWeight: FontWeight.normal
+        ),
+         )
+    );
+  }
+
+}
